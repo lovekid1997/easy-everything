3626ab8dc8fbc6d9003c0134c7a26c13d3afdbf0
VinhNguyen <vinh.nguyenthe@di4l.vn>
2021-06-28 16:07:54 +0700

    test pums

--- test/easy_everything_test.dart
+++ test/easy_everything_test.dart
@@ -1,1 +1,13 @@
-void main() {}
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('description', (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(
+      home: Scaffold(
+        body: Text('Hello'),
+      ),
+    ));
+    expect(find.text('Hello'), findsOneWidget);
+  });
+}
